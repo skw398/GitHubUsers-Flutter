635a5065f2267a7a68e2aec57680f905c1a3a6ae
S.Oshio <skw398@gmail.com>
2023-12-13 15:07:00 +0900

    add widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,58 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:github_users/View/main.dart';
+import 'package:github_users/entity/user.dart';
+import 'package:github_users/provider/github_users_notifier_provider.dart';
+import 'package:mockito/mockito.dart';
+import 'github_users_notifier_test.mocks.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  final mockRepo = MockGitHubUsersRepository();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  Future<void> pumpWidget(WidgetTester tester) async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          gitHubUsersNotifierProvider.overrideWith(() => GitHubUsersNotifier(mockRepo)),
+        ],
+        child: MyApp(),
+      ),
+    );
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  group('GitHubUsersPage Test', () {
+    setUp(() {
+      HttpOverrides.global = null;
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('Success', (WidgetTester tester) async {
+      final users = List.generate(20, (i) => User(
+        id: i,
+        name: "skw398",
+        htmlUrl: Uri.parse("https://github.com/skw398"),
+        avatarUrl: Uri.parse("https://avatars.githubusercontent.com/u/114917347?v=4"),
+      ));
+      when(mockRepo.fetchUsers(userCount: 20, startId: 0)).thenAnswer((_) async => users);
+
+      await pumpWidget(tester);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ListView), findsOneWidget);
+      expect(find.byType(ListTile), findsWidgets);
+    });
+
+    testWidgets('Failure', (WidgetTester tester) async {
+      when(mockRepo.fetchUsers(userCount: 20, startId: 0)).thenThrow(Exception('エラー'));
+
+      await pumpWidget(tester);
+      await tester.pumpAndSettle();
+
+      expect(find.text('再読み込み'), findsOneWidget);
+    });
   });
 }
